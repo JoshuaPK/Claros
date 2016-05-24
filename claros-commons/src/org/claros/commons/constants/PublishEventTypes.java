9c25a764a0640bb9d401c9a380aacf0ce1e60d00
guofeng zhang <guofeng.zhang@mybean.cn>
2016-05-24 16:01:58 +0800

    create pom

--- claros-commons/src/org/claros/commons/constants/PublishEventTypes.java
+++
@@ -1,9 +0,0 @@
-package org.claros.commons.constants;
-
-public class PublishEventTypes {
-	public final static String javascript = "js";
-	public final static String html = "html";
-	public final static String question = "q";
-	
-	
-}
